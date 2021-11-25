3ca13edf4745e4d974eb537064cfef55cf078fa1
Davi Silva <davigomes.ti.ads@gmail.com>
2021-11-25 08:44:32 -0300

    Exercise Workers concluded

---
+++ Program/Program/Entities/HourContract.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Program.Entities
+{
+    class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuerPerHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract(DateTime date, double valuerPerHour, int hours)
+        {
+            Date = date;
+            ValuerPerHour = valuerPerHour;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return Hours * ValuerPerHour;
+        }
+    }
+}
